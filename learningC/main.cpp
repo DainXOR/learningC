e04ba31735a0a88620e84c9a97cbbf7ed7f74e7f
Xaraxx <sara.galvan@udea.edu.co>
2024-05-10 19:25:21 -0500

    hello world code in C++

--- learningC/main.cpp
+++ learningC/main.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
-#include <string>
 
-#include "guide.h"
-
-
-int operator +(int, my_int) {
-	return 2;
-#include "end.h"
-
-int main() {
-	my_int i;
-	i.hola();
-
-	std::cout << i.operator+(3);
-	std::cout << operator+(3, i);
-
-
+int main(){
+	std::cout << "Hello!, fucking world! \n";
 	return 0;
-#include "end.h"
-
+};
