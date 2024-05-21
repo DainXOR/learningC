78cdaeb9feb3f895339258398dbcb1fd8c477cd0
Daniel León <65270757+dainxor@users.noreply.github.com>
2024-05-20 22:36:59 -0500

    1

--- learningC/bottle.hpp
+++ learningC/bottle.hpp
@@ -2,18 +2,3 @@
 #include <iostream>
 #include <string>
 
-template<auto shape>
-class bottle {
-public:
-
-	bottle() {
-	}
-	bottle(int s) {}
-
-	float getVolume() const {
-		return shape.getVolume();
-	}
-
-
-};
-
--- learningC/main.cpp
+++ learningC/main.cpp
@@ -1,70 +1,27 @@
 #include <iostream>
 #include <vector>
-#include <array>
-#include <unordered_map>
+#include <cstdint>
 
-#include "guide.h"
-#include "bottle.hpp"
+struct types {
+	char c;
+	bool b;
 
+	short int s;
 
-int operator +(int, my_int) {
-	return 2;
-}
+	int i1;
+	long int i2;
+	long long int i3;
 
-class hola {
-	std::string name;
-	int i = 0;
-
-	hola() {
-		std::cout << "hola" << std::endl;
-		name = "aaaaaaa";
-	}
-
-public:
-	std::string getName() const {
-		return name;
-	}
-};
-
-// define a hash function for the class hola
-namespace std {
-	template<>
-	struct hash<hola> {
-		std::size_t operator()(const hola& h) const {
-			return std::hash<std::string>()(h.getName());
-		}
-	};
-}
-
-struct cilinder {
-	float radius;
-	float height;
-
-	constexpr float getVolume() const {
-		return 3.1416f * radius * radius * height;
-	}
+	float f;
+	long float lf;
+	double d;
 };
 
 
 int main() {
+	std::vector<int> arr = {};
 
-	my_int i;
-	i.hola();
-
-	std::cout << i.operator+(3);
-	std::cout << operator+(3, i);
-	std::array arr = { 0,1,2,3,4,5,65 };
-	std::vector<my_int> v1 = { my_int(), my_int(), my_int() };
-	int v[] = {1, 2, 3, 4, 5};
-
-	
-	for (auto valor : v)
-		std::cout << valor << std::endl;
-
-	constexpr cilinder sp{ 0, 0 };
-
-	bottle<sp> b;
-	bottle<cilinder{12, 2}> c;
+	arr[13];
 
 	return 0;
 }
